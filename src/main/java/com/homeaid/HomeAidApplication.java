f129f63776d11013f206d7b025984f113007a4d3
Jason Dinh <dinhjdev@gmail.com>
2021-08-18 15:04:15 -0500

    Initial commit

---
+++ src/main/java/com/homeaid/HomeAidApplication.java
@@ -0,0 +1,13 @@
+package com.homeaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeAidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeAidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homeaid/HomeAidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeAidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
