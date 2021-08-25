b6da7876a7d2e2071ae0df6fb354252f0c74b279
Shunya Niizeki <78252315+Shu1020@users.noreply.github.com>
2021-08-25 01:32:14 -0500

    Adding Item and Recipe Services
    Basic CRUD functions

---
+++ ItemService.java
@@ -0,0 +1,32 @@
+package com.homeaid.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.homeaid.models.Item;
+import com.homeaid.repositories.ItemRepository;
+
+@Service
+public class ItemService {
+	@Autowired
+	ItemRepository itemRepository;
+	
+	public List<Item> allItem(){
+		return this.itemRepository.findAll();
+	}
+	
+	public Item createItem (Item item) {
+		return this.itemRepository.save(item);
+	}
+	
+	public Item updateItem (Long id, Item item) {
+		return this.itemRepository.save(item);
+	}
+	
+	public void deleteItem(Long id) {
+		this.itemRepository.deleteById(id);
+	}
+	
+}
